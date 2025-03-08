54480bf7c36d96eea70a4e57b32c2fd008b6d34c
ludovicmantovani <ludovic.mantov@gmail.com>
2025-03-08 17:49:59 +0100

    [ADD]: Command line argument parse for input file (-f)

--- Main.qml
+++ Main.qml
@@ -1,8 +1,17 @@
 import QtQuick
+import QtQuick.Controls
 
-Window {
+ApplicationWindow {
+    id: root
     width: 1024
     height: 640
     visible: true
     title: qsTr("Viewer3D")
+
+    Text {
+        anchors.centerIn: parent
+        text: filePath ? "File: " + filePath : "No file provided"
+        color: "black"
+        font.pixelSize: 20
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,17 +1,53 @@
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QFile>
+#include <QFileInfo>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QString>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Viewer3D Application");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption fileOption(QStringList() << "f" << "file",
+                                  "Path to the file to load", "filePath");
+    parser.addOption(fileOption);
+
+    parser.process(app);
+
+    // Retrieve file path from arguments
+    QString filePath;
+
+    QString currentFilePath = parser.value(fileOption);
+
+    QFileInfo fileInfo(currentFilePath);
+    currentFilePath = fileInfo.absoluteFilePath();
+
+    // Check if the file exists
+    QFile file(currentFilePath);
+    if (file.exists() && file.open(QIODevice::ReadOnly)) {
+        filePath = currentFilePath;
+
+        // Log info about the file
+        qInfo() << "File is valid and can be read:" << filePath;
+        file.close(); // Close the file after checking
+    } else {
+        qWarning() << "File does not exist or cannot be read:" << currentFilePath;
+    }
+
+    // Expose filePath to QML
+    engine.rootContext()->setContextProperty("filePath", filePath);
     QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+        &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+        []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
     engine.loadFromModule("Viewer3D", "Main");
 
     return app.exec();
