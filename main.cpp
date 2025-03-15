eee42b0d60e184d8b90bf8081d1149492b38ac2d
ludovicmantovani <ludovic.mantov@gmail.com>
2025-03-15 12:33:42 +0100

    Test file creation

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QString>
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
 
   std::cout << "HELLO WORLD !!!!" << std::endl;
+
+  std::ofstream outfile("test.txt");
+
+  outfile << "my text here!" << std::endl;
+
+  outfile.close();
+
   return 0;
   // QGuiApplication app(argc, argv);
 
